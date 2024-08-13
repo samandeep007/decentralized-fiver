79c51e6fee356607ef4a8b97cfffda7b03338a47
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-12 22:17:57 -0600

    refactor: Add options field to Task model

--- backend/src/models/task.model.js
+++ backend/src/models/task.model.js
@@ -1,13 +1,24 @@
 import mongoose from 'mongoose';
 
+const Option = new mongoose.Schema({
+    image_url: String,
+    option_id: Number,
+    task_id: {
+        type: mongoose.Schema.Types.ObjectId(),
+        ref: "Task"
+    }
+})
+
 const TaskSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
         trim: true
-    }
+    },
+
+    options: Array<Option>
 }, { timestamps: true });
 
-const UserModel = mongoose.models.tasks || mongoose.model('Task', TaskSchema);
+const TaskModel = mongoose.models.tasks || mongoose.model('Task', TaskSchema);
 
-export default UserModel;
+export default TaskModel;
